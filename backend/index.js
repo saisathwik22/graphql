a233f974ca4eec9e461888a9854c587b9838cfd2
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-29 19:22:11 +0530

    express middleware configured. CodeStage - 43:00

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { resolve } from "path";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+// Ensure we wait for our server to start
+await server.start();
 
-console.log(`Server ready at ${url}`);
+// set up our express middleware to handle CORS, body parsing, and our expressMiddleware function
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`Server ready at http://localhost:4000/`);

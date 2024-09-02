24f962d2e1cb62a3a368a8611f6634bd83b6e651
saisathwik22 <reddysaisathwik022@gmail.com>
2024-09-02 08:47:51 +0530

    Pages initiated and imported into App.jsx CodeStage - 1:51:30

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
+  const authUser = true;
   return (
     <>
-      <h1 className='text-red-500 text-5xl bg-blue-500'>Hello</h1>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/transaction/:id' element={<TransactionPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
